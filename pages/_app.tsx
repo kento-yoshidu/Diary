2257ed4ce156cbca7b94e2d55153387d86e56b6a
kento <kento@toriwatari.bird>
2023-01-21 13:51:00 +0900

    ✨ create global.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import Layout from '@/components/layout'
 import type { AppProps } from 'next/app'
 
+import "styles/global.css"
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
