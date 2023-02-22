fe3dc2561526141fa5ca5c60b5ec9adb49fb5db3
kento <kento@toriwatari.bird>
2023-02-22 23:02:34 +0900

    ✨ Update

---
+++ components/postHeader.tsx
@@ -0,0 +1,25 @@
+import styles from "styles/post-header.module.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faClock } from "@fortawesome/free-regular-svg-icons"
+
+type Props = {
+  title: string,
+  subtitle: string,
+  publish: string
+}
+
+export default function PostHeader({ title, subtitle, publish = ""}: Props) {
+  return (
+    <div className={styles.stack}>
+      <p className={styles.subtitle}>{subtitle}</p>
+      <h1 className={styles.title}>{title}</h1>
+
+      {publish && (
+        <div className={styles.publish}>
+          <FontAwesomeIcon icon={faClock} size="lg" color="var(--gray-25)" />
+          {publish}
+        </div>
+      )}
+    </div>
+  )
+}
