10ab69f9de4987c83d42158690e6831d24f55c14
kento <kento@toriwatari.bird>
2023-02-26 12:35:32 +0900

    ✨ 記事一覧に画像を表示

--- components/posts.tsx
+++ components/posts.tsx
@@ -1,5 +1,6 @@
 import styles from "styles/posts.module.css"
-import Link from "next/link";
+import Link from "next/link"
+import Image from "next/image"
 
 import type { Post } from "@/types/types";
 
@@ -10,10 +11,25 @@ type Props = {
 export default function Posts({ posts }: Props) {
   return (
     <div className={styles.gridContainer}>
-      {posts.map(({ title, slug }) => (
+      {posts.map(({ title, slug, eyecatch }) => (
         <article className={styles.post} key={slug}>
           <Link href={`/blog/${slug}`}>
-            <h2>{title}</h2>
+            <figure>
+              <Image
+                src={eyecatch.url}
+                alt=""
+                //layout="fill"
+                //objectFit="cover"
+                layout="responsive"
+                width={eyecatch.width}
+                height={eyecatch.height}
+                sizes="(min-width: 1152px) 576px, 50vw"
+                placeholder="blur"
+                blurDataURL={eyecatch.blurDataURL}
+              />
+              <h2>{title}</h2>
+
+            </figure>
           </Link>
         </article>
       ))}
