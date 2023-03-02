22f6fca32f21f7e0b036974a2ac62c85fb196c7c
kento <kento@toriwatari.bird>
2023-03-02 20:07:06 +0900

    ✨ Update

--- styles/accordion.module.css.d.ts
+++ styles/accordion.module.css.d.ts
@@ -1,9 +1,10 @@
 declare const styles: {
   readonly "open": string;
+  readonly "close": string;
+  readonly "text": string;
   readonly "heading": string;
   readonly "icon": string;
   readonly "textInner": string;
-  readonly "text": string;
 };
 export = styles;
 
