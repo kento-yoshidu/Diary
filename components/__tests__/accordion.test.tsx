62e1f18a3260d0fd4a2f84c238ee9b85b90e3555
kento <kento@toriwatari.bird>
2023-03-03 21:26:40 +0900

    ✨ Update accordion.test.tsx

--- components/__tests__/accordion.test.tsx
+++ components/__tests__/accordion.test.tsx
@@ -5,24 +5,26 @@ import '@testing-library/jest-dom/extend-expect'
 import Accordion from "../accordion"
 
 describe("Accordion test", () => {
+  const component = <Accordion heading="dummy header">dummy text</Accordion>
+
   it("Rendering According Component", () => {
-    render(<Accordion heading="dummy header">dummy text</Accordion>)
+    render(component)
     expect(screen.getByRole("button")).toHaveTextContent("dummy header")
   })
 
   it("Close class is given to the div element", () => {
-    render(<Accordion heading="dummy header">dummy text</Accordion>)
+    render(component)
     expect(screen.getByTestId("div")).toHaveClass("close")
   })
 
   it("When the button is clicked, the open class is given", async () => {
-    render(<Accordion heading="dummy header">dummy text</Accordion>)
+    render(component)
     await userEvent.click(screen.getByRole("button"))
     expect(screen.getByTestId("div")).toHaveClass("open")
   })
 
   it("When the button is clicked, the close class is not given", async () => {
-    render(<Accordion heading="dummy header">dummy text</Accordion>)
+    render(component)
     await userEvent.click(screen.getByRole("button"))
     expect(screen.getByTestId("div")).not.toHaveClass("close")
   })
